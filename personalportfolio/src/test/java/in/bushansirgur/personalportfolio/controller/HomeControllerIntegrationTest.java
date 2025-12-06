b8ed2c888ab5b7dba3b5a48ec50bd105ca6057a8
AKASH <akask.kush@gmail.com>
2025-12-06 12:48:29 +0530

    Port1

--- personalportfolio/src/test/java/in/bushansirgur/personalportfolio/controller/HomeControllerIntegrationTest.java
+++ personalportfolio/src/test/java/in/bushansirgur/personalportfolio/controller/HomeControllerIntegrationTest.java
@@ -1,5 +1,4 @@
 package in.bushansirgur.personalportfolio.controller;
-
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -12,14 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-
 @SpringBootTest
 @AutoConfigureMockMvc
 class HomeControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
-
     @Test
     @DisplayName("GET /home renders the home view with the page title")
     void homePageRendersWithExpectedTitle() throws Exception {
